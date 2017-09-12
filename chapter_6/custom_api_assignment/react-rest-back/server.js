5d986434083a0c0ac6161064aee62be412ad3ebc
Chris Thamo <chrisdev2016@users.noreply.github.com>
2017-09-12 14:00:56 -0400

    code cleanup

--- chapter_6/custom_api_assignment/react-rest-back/server.js
+++ chapter_6/custom_api_assignment/react-rest-back/server.js
@@ -1,44 +1,44 @@
 const express = require('express');
-const bodyParser =require('body-parser')
+const bodyParser = require('body-parser')
 
-const app =express();
+const app = express();
 
 
-var listItems=[
-	            {text: 'learn angular', done: false, id: 1},
-	            {text: 'write the content for the next module', done: false, id: 2},
-	            {text: 'buy cheese', done: true, id: 3},
-	            {text: 'buy milk', done: true, id: 4}
-			]
+var listItems = [
+    { text: 'learn angular', done: false, id: 1 },
+    { text: 'write the content for the next module', done: false, id: 2 },
+    { text: 'buy cheese', done: true, id: 3 },
+    { text: 'buy milk', done: true, id: 4 }
+]
 
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
 
-app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin","*");
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type,Accept");
     next();
 })
 
 
 
-app.get('/todos',  function(req,res){
+app.get('/todos', function (req, res) {
     res.json(listItems)
 })
 
 
-app.post('/todos', (req, res)=>{
+app.post('/todos', (req, res) => {
 
-   listItems  = req.body.list
-   res.json(listItems)
+    listItems = req.body.list
+    res.json(listItems)
 
 })
 
 
 
-app.listen(8080, ()=>{
+app.listen(8080, () => {
     console.log("server listening on port 8080")
 })
 
