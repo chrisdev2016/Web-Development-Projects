c318e60ac69fc0049b7533fa67a83951fabe3cd3
Chris Thamo <chrisdev2016@users.noreply.github.com>
2017-09-12 13:56:07 -0400

    Node assignmentss completed

--- chapter_3/express_assignment/index.js
+++ chapter_3/express_assignment/index.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var app = express();
 
-app.get('/joke', function(req, res){
+
+app.get('/joke', function (req, res) {
 
     res.sendFile(__dirname + '/joke.html');
 })
 
 app.use(express.static('subdirectory'));
 
-app.get('/journal', function(req, res){
-    var cacheTime =(2*24*60*60);
-    var options = {maxAge:cacheTime};
+app.get('/journal', function (req, res) {
+    var cacheTime = (2 * 24 * 60 * 60);
+    var options = { maxAge: cacheTime };
 
     res.sendFile(__dirname + '/subdirectory/index.html', options);
 })
+
 app.listen(8080);
